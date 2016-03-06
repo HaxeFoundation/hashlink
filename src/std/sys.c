fccb4f128d312b2b9277391bbbc176d3a7746bc5
Nicolas Cannasse <ncannasse@gmail.com>
2016-03-06 23:52:14 +0100

    added std/file, more std/sys

--- src/std/sys.c
+++ src/std/sys.c
@@ -1,9 +1,55 @@
+/*
+ * Copyright (C)2005-2016 Haxe Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 #include <hl.h>
 
 #ifndef HL_WIN
 #	include <sys/time.h>
 #	include <sys/times.h>
+#	define HL_UTF8PATH
 #endif
+
+HL_PRIM bool hl_sys_utf8_path() {
+#ifdef HL_UTF8_PATH
+	return true;
+#else
+	return false;
+#endif
+}
+
+HL_PRIM vbyte *hl_sys_string() {
+#if defined(HL_WIN)
+	return (vbyte*)USTR("Windows");
+#elif defined(NEKO_GNUKBSD)
+	return (vbyte*)USTR("GNU/kFreeBSD");
+#elif defined(NEKO_LINUX)
+	return (vbyte*)USTR("Linux");
+#elif defined(NEKO_BSD)
+	return (vbyte*)USTR("BSD");
+#elif defined(NEKO_MAC)
+	return (vbyte*)USTR("Mac");
+#else
+#error Unknow system string
+#endif
+}
 
 HL_PRIM void hl_sys_print( vbyte *msg ) {
 	uprintf(USTR("%s"),(uchar*)msg);
