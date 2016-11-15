38ad4d1e0124e9fb4c79541f5b78d5bc3d83b723
Andy Li <andy@onthewings.net>
2016-11-15 14:07:10 +0800

    added initial cmake config that can compiles libhl and hl

---
+++ cmake/linux32.toolchain.cmake
@@ -0,0 +1,10 @@
+# Toolchain file for building a 32bit version on a 64bit host.
+#
+# Usage:
+# cmake -DCMAKE_TOOLCHAIN_FILE=cmake/linux32.toolchain.cmake <sourcedir>
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
