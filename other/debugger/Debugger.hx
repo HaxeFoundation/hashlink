1f0df2048d429efe6b7aef8dd47a81a4fc19b1ab
Nicolas Cannasse <ncannasse@gmail.com>
2016-12-08 00:47:42 +0100

    refactor

--- other/debugger/Debugger.hx
+++ other/debugger/Debugger.hx
@@ -1,38 +1,5 @@
 import HLReader;
-
-@:enum abstract WaitResult(Int) {
-	public var Timeout = -1;
-	public var Exit = 0;
-	public var Breakpoint = 1;
-	public var SingleStep = 2;
-	public var Error = 3;
-}
-
-@:enum abstract Register(Int) {
-	public var Esp = 0;
-	public var Ebp = 1;
-	public var Eip = 2;
-	public var EFlags = 3;
-	public var Dr0 = 4;
-	public var Dr1 = 5;
-	public var Dr2 = 6;
-	public var Dr3 = 7;
-	public var Dr6 = 8;
-	public var Dr7 = 9;
-}
-
-extern class DebugApi {
-	@:hlNative("std", "debug_start") public static function startDebugProcess( pid : Int ) : Bool;
-	@:hlNative("std", "debug_stop") public static function stopDebugProcess( pid : Int ) : Void;
-	@:hlNative("std", "debug_breakpoint") public static function breakpoint( pid : Int ) : Bool;
-	@:hlNative("std", "debug_read") public static function read( pid : Int, ptr : hl.Bytes, buffer : hl.Bytes, size : Int ) : Bool;
-	@:hlNative("std", "debug_write") public static function write( pid : Int, ptr : hl.Bytes, buffer : hl.Bytes, size : Int ) : Bool;
-	@:hlNative("std", "debug_flush") public static function flush( pid : Int, ptr : hl.Bytes, size : Int ) : Bool;
-	@:hlNative("std", "debug_wait") public static function wait( pid : Int, threadId : hl.Ref<Int>, timeout : Int ) : WaitResult;
-	@:hlNative("std", "debug_resume") public static function resume( pid : Int, tid : Int ) : Bool;
-	@:hlNative("std", "debug_read_register") public static function readRegister( pid : Int, tid : Int, register : Register ) : hl.Bytes;
-	@:hlNative("std", "debug_write_register") public static function writeRegister( pid : Int, tid : Int, register : Register, v : Pointer ) : Bool;
-}
+import DebugApi;
 
 enum DebugValueRepr {
 	VUndef;
@@ -70,67 +37,6 @@ typedef DebugObj = {
 	}>;
 }
 
-abstract Pointer(hl.Bytes) to hl.Bytes {
-
-	static var TMP = new hl.Bytes(8);
-
-	public inline function new(b) {
-		this = b;
-	}
-
-	public inline function offset(pos:Int) : Pointer {
-		return new Pointer(this.offset(pos));
-	}
-
-	public inline function sub( p : Pointer ) {
-		return this.subtract(p);
-	}
-
-	public inline function or( v : Int ) {
-		return new Pointer(hl.Bytes.fromAddress(this.address() | v));
-	}
-
-	public inline function and( v : Int ) {
-		return new Pointer(hl.Bytes.fromAddress(this.address() & v));
-	}
-
-	public function readByte( offset : Int ) {
-		if( !DebugApi.read(Debugger.PID, this.offset(offset), TMP, 1) )
-			throw "Failed to read @" + offset;
-		return TMP.getUI8(0);
-	}
-
-	public function writeByte( offset : Int, value : Int ) {
-		TMP.setUI8(0, value);
-		if( !DebugApi.write(Debugger.PID, this.offset(offset), TMP, 1) )
-			throw "Failed to write @" + offset;
-	}
-
-	public function flush( pos : Int ) {
-		DebugApi.flush(Debugger.PID, this.offset(pos), 1);
-	}
-
-	public function toInt() {
-		return this.address().low;
-	}
-
-	public function toString() {
-		var i = this.address();
-		if( i.high == 0 )
-			return "0x" + StringTools.hex(i.low);
-		return "0x" + StringTools.hex(i.high) + StringTools.hex(i.low, 8);
-	}
-
-	public static function ofPtr( p : hl.Bytes ) : Pointer {
-		return cast p;
-	}
-
-	public static function make( low : Int, high : Int ) {
-		return new Pointer(hl.Bytes.fromAddress(haxe.Int64.make(high, low)));
-	}
-
-}
-
 typedef GlobalAccess = {
 	var sub : Map<String,GlobalAccess>;
 	var gid : Null<Int>;
@@ -138,9 +44,9 @@ typedef GlobalAccess = {
 
 class Debugger {
 
-	public static var PID = 0;
 	static inline var INT3 = 0xCC;
 
+	var pid : Int;
 	var code : HLCode;
 	var fileIndexes : Map<String, Int>;
 	var functionsByFile : Map<Int, Array<{ f : HLFunction, ifun : Int, lmin : Int, lmax : Int }>>;
@@ -376,7 +282,8 @@ class Debugger {
 
 	public function startDebug( pid : Int, port : Int ) {
 
-		PID = pid;
+		this.pid = pid;
+		Pointer.PID = pid;
 		sock = new sys.net.Socket();
 		try {
 			sock.connect(new sys.net.Host("127.0.0.1"), port);
@@ -413,7 +320,7 @@ class Debugger {
 		var tid = 0;
 		var cmd = Timeout;
 		while( true ) {
-			cmd = DebugApi.wait(PID, tid, 1000);
+			cmd = DebugApi.wait(pid, tid, 1000);
 			switch( cmd ) {
 			case Timeout:
 				// continue
@@ -453,7 +360,7 @@ class Debugger {
 	}
 
 	public function resume() {
-		if( !DebugApi.resume(PID, stoppedThread) )
+		if( !DebugApi.resume(pid, stoppedThread) )
 			throw "Could not resume "+stoppedThread;
 		stoppedThread = null;
 	}
@@ -645,7 +552,7 @@ class Debugger {
 	}
 
 	function getReg(tid, reg) {
-		return Pointer.ofPtr(DebugApi.readRegister(PID, tid, reg));
+		return Pointer.ofPtr(DebugApi.readRegister(pid, tid, reg));
 	}
 
 	function readReg(frame, index) {
@@ -680,13 +587,13 @@ class Debugger {
 	}
 
 	function setReg(tid, reg, value) {
-		if( !DebugApi.writeRegister(PID, tid, reg, value) )
+		if( !DebugApi.writeRegister(pid, tid, reg, value) )
 			throw "Failed to set register " + reg;
 	}
 
 	function readMem( p : Pointer, size : Int ) {
 		var mem = new hl.Bytes(size);
-		if( !DebugApi.read(PID, p, mem, size) )
+		if( !DebugApi.read(pid, p, mem, size) )
 			throw "Failed to read memory @" + p.toString() + "[" + size+"]";
 		return mem;
 	}
