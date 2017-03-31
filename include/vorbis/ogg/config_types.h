639b046a258c634cbebec0b3a460781bc730e2be
Nicolas Cannasse <ncannasse@gmail.com>
2017-03-31 15:22:10 +0200

    more warnings fixes

---
+++ include/vorbis/ogg/config_types.h
@@ -0,0 +1,17 @@
+
+// include by os_types.h as a fallback
+
+#if defined(__ORBIS__)
+
+#  include <sys/types.h>
+typedef short ogg_int16_t;
+typedef unsigned short ogg_uint16_t;
+typedef int ogg_int32_t;
+typedef unsigned int ogg_uint32_t;
+typedef long long ogg_int64_t;
+
+#else
+
+#error "Could not tell which OS config we compile to"
+
+#endif
