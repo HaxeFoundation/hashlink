1a080f45440169fa160c2f5f58d8c957392b5bb3
ncannasse <ncannasse@gmail.com>
2017-09-27 17:10:44 +0200

    added 64 bits detection with __LP64__ (osx/ios)

--- src/hl.h
+++ src/hl.h
@@ -45,7 +45,7 @@
 #	define HL_BSD
 #endif
 
-#if defined(_64BITS) || defined(__x86_64__) || defined(_M_X64)
+#if defined(_64BITS) || defined(__x86_64__) || defined(_M_X64) || defined(__LP64__)
 #	define HL_64
 #endif
 
