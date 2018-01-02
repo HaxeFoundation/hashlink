955d11a6806eb37a618b8f403aa19868f62b28e0
Andy Li <andy@onthewings.net>
2018-01-02 16:35:30 +0800

    fix uchar for Mac

--- src/hl.h
+++ src/hl.h
@@ -182,6 +182,10 @@ typedef wchar_t	uchar;
 #	define utoi(s,end)	wcstol(s,end,10)
 #	define ucmp(a,b)	wcscmp(a,b)
 #	define utostr(out,size,str) wcstombs(out,str,size)
+#elif defined(HL_MAC)
+typedef uint16_t uchar;
+#	undef USTR
+#	define USTR(str)	u##str
 #else
 #	include <stdarg.h>
 #	include <uchar.h>
