8d554d47a80fd57ae4f6554bef8edd62629743d5
Dan Korostelev <nadako@gmail.com>
2018-02-07 01:05:15 +0300

    only use FindLibUV on unix (also fix the cmake file)

--- cmake/FindLibUV.cmake
+++ cmake/FindLibUV.cmake
@@ -98,7 +98,7 @@ unset(_LibUV_H_REGEX)
 unset(_LibUV_H)
 
 #-----------------------------------------------------------------------------
-include(${CMAKE_CURRENT_LIST_DIR}/../../Modules/FindPackageHandleStandardArgs.cmake)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibUV
   FOUND_VAR LibUV_FOUND
   REQUIRED_VARS LibUV_LIBRARY LibUV_INCLUDE_DIR
