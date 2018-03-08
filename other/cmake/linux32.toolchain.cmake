f02403ed20a7e44d6099e662638fd7fbd2c4a887
Andy Li <andy@onthewings.net>
2018-03-08 17:07:37 +0800

    [TravisCI] toolchain file was moved

--- other/cmake/linux32.toolchain.cmake
+++ other/cmake/linux32.toolchain.cmake
@@ -1,7 +1,7 @@
 # Toolchain file for building a 32bit version on a 64bit host.
 #
 # Usage:
-# cmake -DCMAKE_TOOLCHAIN_FILE=cmake/linux32.toolchain.cmake <sourcedir>
+# cmake -DCMAKE_TOOLCHAIN_FILE=other/cmake/linux32.toolchain.cmake <sourcedir>
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
