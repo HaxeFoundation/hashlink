a7bf1211e28d9b6140590fe4897f2b0cdbfa363f
Nicolas Cannasse <ncannasse@gmail.com>
2018-04-14 16:42:48 +0200

    fixed allocation done in hl_blocking

--- src/std/file.c
+++ src/std/file.c
@@ -159,7 +159,9 @@ HL_PRIM vbyte *hl_file_contents( vbyte *name, int *size ) {
 	len = ftell(f);
 	if( size ) *size = len;
 	fseek(f,0,SEEK_SET);
+	hl_blocking(false);
 	content = (vbyte*)hl_gc_alloc_noptr(size ? len : len+1);
+	hl_blocking(true);
 	if( !size ) content[len] = 0; // final 0 for UTF8
 	while( len > 0 ) {
 		int d = (int)fread((char*)content + p,1,len,f);
