753e3d77a9018bd0e2733ec83fa7d26816b80984
ncannasse <ncannasse@gmail.com>
2018-05-09 18:15:59 +0200

    added disposeDriver

--- libs/directx/dx/Driver.hx
+++ libs/directx/dx/Driver.hx
@@ -464,7 +464,7 @@ class ShaderResourceViewDesc {
 class Driver {
 
 	public static var fullScreen(get, set) : Bool;
-	
+
 	/**
 		Setup an error handler instead of getting String exceptions:
 		The first parameter is the DirectX error code
@@ -477,6 +477,9 @@ class Driver {
 
 	public static function create( win : Window, format : Format, flags : DriverInitFlags = None, restrictLevel = 0 ) {
 		return dxCreate(@:privateAccess win.win, format, flags, restrictLevel);
+	}
+
+	public static function disposeDriver( driver : DriverInstance ) {
 	}
 
 	public static function resize( width : Int, height : Int, format : Format ) : Bool {
