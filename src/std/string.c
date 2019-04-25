b506e0296129f555805fdd938655a4d6bed4a2cf
Aurel Bílý <aurel.bily@gmail.com>
2019-04-25 23:37:07 +0100

    fix utf16_to_utf8 and url_encode

--- src/std/string.c
+++ src/std/string.c
@@ -225,7 +225,7 @@ HL_PRIM vbyte *hl_utf16_to_utf8( vbyte *str, int len, int *size ) {
 			out[p++] = (vbyte)(0xC0|(v>>6));
 			out[p++] = (vbyte)(0x80|(v&63));
 		} else if( v >= 0xD800 && v <= 0xDFFF ) {
-			int k = ((((int)v - 0xD800) << 10) | (((int)*++c) - 0xDC00)) + 0x10000;
+			int k = ((((int)v - 0xD800) << 10) | (((int)*++c) - 0xDC00));
 			out[p++] = (vbyte)(0xF0|(k>>18));
 			out[p++] = (vbyte)(0x80 | ((k >> 12) & 63));
 			out[p++] = (vbyte)(0x80 | ((k >> 6) & 63));
@@ -272,7 +272,7 @@ HL_PRIM vbyte *hl_url_encode( vbyte *str, int *len ) {
 				sur = (unsigned)*cstr;
 				if( sur >= 0xDC00 && sur < 0xDFFF ) {
 					cstr++;
-					c = ((((int)c - 0xD800) << 10) | ((int)sur - 0xDC00)) + 0x10000;
+					c = ((((int)c - 0xD800) << 10) | ((int)sur - 0xDC00));
 					hl_buffer_hex(b, 0xF0|(c>>18));
 					hl_buffer_hex(b, 0x80|((c >> 12) & 63));
 					hl_buffer_hex(b, 0x80|((c >> 6) & 63));
