96f6eb0002458e7a2440b6dd525c4745da765d4e
Nicolas Cannasse <ncannasse@gmail.com>
2019-11-18 23:43:26 +0100

    more work on multiple frames timing

--- other/profiler/ProfileGen.hx
+++ other/profiler/ProfileGen.hx
@@ -48,7 +48,7 @@ class StackLink {
 
 class Frame {
 	public var samples : Array<{ thread : Int, time : Float, stack : Array<StackElement> }> = [];
-	public var endTime : Null<Float>;
+	public var startTime : Float;
 	public function new() {
 	}
 }
@@ -83,18 +83,37 @@ class ProfileGen {
 	}
 
 	static function main() {
-		var file = Sys.args()[0];
+		var args = Sys.args();
+		var outFile = null;
+		var debug = false;
+
+		while( args.length > 0 ) {
+			var arg = args[0];
+			if( arg.charCodeAt(0) != "-".code ) continue;
+			args.shift();
+			switch( arg ) {
+			case "-debug":
+				debug = true;
+			case "-out":
+				outFile = args.shift();
+			default:
+				throw "Unknown parameter "+arg;
+			}
+		}
+
+		var file = args.shift();
 		if( file == null ) file = "hlprofile.dump";
 		if( sys.FileSystem.isDirectory(file) ) file += "/hlprofile.dump";
+		if( outFile == null ) outFile = file;
 
 		var f = sys.io.File.read(file);
 		if( f.readString(4) != "PROF" ) throw "Invalid profiler file";
 		var version = f.readInt32();
 		var sampleCount = f.readInt32();
-		var cache = new Map();
 		var rootElt = new StackElement("(root)");
 		var curFrame = new Frame();
 		var frames = [curFrame];
+		var fileMaps : Array<Map<Int,StackElement>> = [];
 		while( true ) {
 			var time = try f.readDouble() catch( e : haxe.io.Eof ) break;
 			var tid = f.readInt32();
@@ -110,7 +129,7 @@ class ProfileGen {
 					var elt : StackElement;
 					if( file < 0 ) {
 						file &= 0x7FFFFFFF;
-						elt = cache.get(file+"-"+line);
+						elt = fileMaps[file].get(line);
 						if( elt == null ) throw "assert";
 					} else {
 						var len = f.readInt32();
@@ -118,7 +137,12 @@ class ProfileGen {
 						for( i in 0...len ) buf.addChar(f.readUInt16());
 						var str = buf.toString();
 						elt = new StackElement(str);
-						cache.set(file+"-"+line, elt);
+						var m = fileMaps[file];
+						if( m == null ) {
+							m = new Map();
+							fileMaps[file] = m;
+						}
+						m.set(line,elt);
 					}
 					stack[i] = elt;
 				}
@@ -128,8 +152,8 @@ class ProfileGen {
 				var data = f.read(size);
 				switch( msgId ) {
 				case 0:
-					curFrame.endTime = time;
 					curFrame = new Frame();
+					curFrame.startTime = time;
 					frames.push(curFrame);
 				default:
 					Sys.println("Unknown profile message #"+msgId);
@@ -139,9 +163,10 @@ class ProfileGen {
 
 		var s0 = frames[0].samples[0];
 		var tid = s0.thread;
+		frames[0].startTime = s0.time;
 
 		function timeStamp(t:Float) {
-			return Std.int((t - s0.time) * 1000000);
+			return Std.int((t - s0.time) * 1000000) + 1;
 		}
 
 		var json : Array<Dynamic> = [
@@ -163,9 +188,25 @@ class ProfileGen {
 			    name : "Profile",
 				id : "0x1",
 				args: { data : { startTime : 0 } },
+			},
+			{
+				pid : 0,
+				tid : tid,
+				ts : 0,
+				ph : "B",
+				cat : "devtools.timeline",
+				name : "FunctionCall",
+			},
+			{
+				pid : 0,
+				tid : tid,
+				ts : 1,
+				ph : "E",
+				cat : "devtools.timeline",
+				name : "FunctionCall"
 			}
 		];
-		var lastT = s0.time - 1 / sampleCount;
+		var lastT = s0.time;
 		var rootStack = new StackLink(rootElt);
 
 		for( f in frames ) {
@@ -173,7 +214,7 @@ class ProfileGen {
 			json.push({
 				pid : 0,
 				tid : tid,
-				ts : timeStamp(f.samples[0].time),
+				ts : timeStamp(f.startTime),
 				ph : "B",
 				cat : "devtools.timeline",
 				name : "FunctionCall",
@@ -181,7 +222,7 @@ class ProfileGen {
 			json.push({
 				pid : 0,
 				tid : tid,
-				ts : timeStamp(f.endTime == null ? f.samples[f.samples.length-1].time : f.endTime),
+				ts : timeStamp(f.samples[f.samples.length-1].time),
 				ph : "E",
 				cat : "devtools.timeline",
 				name : "FunctionCall"
@@ -192,15 +233,18 @@ class ProfileGen {
 
 			var timeDeltas = [];
 			var allStacks = [];
+			var lines = [];
 
 			for( s in f.samples) {
 				var st = rootStack;
-				var start = 0;
-				for( i in start...s.stack.length ) {
+				var line = 0;
+				for( i in 0...s.stack.length ) {
 					var s = s.stack[s.stack.length - 1 - i];
 					if( s == null || s.file == "?" ) continue;
+					line = s.line;
 					st = st.getChildren(s);
 				}
+				lines.push(line);
 				allStacks.push(st);
 				timeDeltas.push(Std.int((s.time - lastT)*1000000));
 				lastT = s.time;
@@ -218,14 +262,14 @@ class ProfileGen {
 						cpuProfile : {
 							nodes : makeStacks(allStacks),
 							samples : [for( s in allStacks ) s.id],
+							//lines : lines,
 						},
 						timeDeltas : timeDeltas,
 					}
 				}
 			});
-			lastT = f.endTime;
 		}
-		sys.io.File.saveContent("Profile.json", haxe.Json.stringify(json,"\t"));
+		sys.io.File.saveContent(outFile, debug ? haxe.Json.stringify(json,"\t") : haxe.Json.stringify(json));
 	}
 
 }
