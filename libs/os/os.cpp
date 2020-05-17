2bfa52b3bfb27e4a40ff65c3084878e8c9bf1cdb
Taneli Heikkinen <trikafka@gmail.com>
2020-05-18 00:00:29 +0300

    Changes. Renamed clip to OS for now, removed unzip. Added clipboard image functions.

---
+++ libs/os/os.cpp
@@ -0,0 +1,75 @@
+#define HL_NAME(n) os_##n
+#include <hl.h>
+
+#include <clip.h>
+
+HL_PRIM const char * HL_NAME(get_clipboard_text)() {
+    std::string text;
+    clip::get_text(text);
+    return text.c_str();
+}
+
+HL_PRIM bool HL_NAME(set_clipboard_text)(const char * text) {
+    return clip::set_text(text);    
+}
+
+HL_PRIM vbyte * HL_NAME(get_clipboard_image_data)() {
+    clip::image img;
+    if(clip::get_image(img)) {
+        int size = img.spec().width * img.spec().height * (img.spec().bits_per_pixel / 8) * sizeof(char);
+        vbyte * data = (vbyte *)hl_gc_alloc_raw(size);
+        memcpy(data, img.data(), size);
+        return (vbyte *) data;
+    }
+
+    return NULL;
+}   
+
+HL_PRIM bool HL_NAME(set_clipboard_image)(char * data, int w, int h, int bpp, int bpr, int rmask, int gmask, int bmask, int amask, int rshift, int gshift, int bshift, int ashift) {
+    clip::image_spec imgS;
+    imgS.width = w;
+    imgS.height = h;
+    imgS.bits_per_pixel = bpp;
+    imgS.bytes_per_row = bpr;
+    imgS.red_mask = rmask;
+    imgS.green_mask = gmask;
+    imgS.blue_mask = bmask;
+    imgS.alpha_mask = amask;
+    imgS.red_shift = rshift;
+    imgS.green_shift = gshift;
+    imgS.blue_shift = bshift;
+    imgS.alpha_shift = ashift;
+
+    clip::image img(data, imgS);
+    return clip::set_image(img);
+}
+
+HL_PRIM bool HL_NAME(get_clipboard_image_spec)(int * w, int * h, int * bpp, int * bpr, int * rmask, int * gmask, int * bmask, int * amask, int * rshift, int * gshift, int * bshift, int * ashift) {
+    clip::image_spec spec;
+    if(clip::get_image_spec(spec)) {
+        *w = (int) spec.width;
+        *h = (int) spec.height;
+        *bpp = (int) spec.bits_per_pixel;
+        *bpr = (int) spec.bytes_per_row;
+        *rmask = (int) spec.red_mask;
+        *gmask = (int) spec.green_mask;
+        *bmask = (int) spec.blue_mask;
+        *amask = (int) spec.alpha_mask;
+        *rshift = (int) spec.red_shift;
+        *gshift = (int) spec.green_shift;
+        *bshift = (int) spec.blue_shift;
+        *ashift = (int) spec.alpha_shift;
+
+        return true;
+    }
+
+    return false;
+}
+
+#define _IMAGE_SPEC _ABSTRACT(clip::image_spec)
+DEFINE_PRIM(_BYTES, get_clipboard_text, _NO_ARG);
+DEFINE_PRIM(_BOOL, set_clipboard_text, _BYTES);
+DEFINE_PRIM(_BYTES, get_clipboard_image_data, _NO_ARG);
+DEFINE_PRIM(_BOOL, set_clipboard_image, _BYTES _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32);
+DEFINE_PRIM(_BOOL, get_clipboard_image_spec, _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32));
+
---
+++ libs/os/os/Clipboard.hx
@@ -0,0 +1,86 @@
+package os;
+
+typedef ClipboardImageSpec = {
+	width : Int,
+	height : Int,
+	bitsPerPixel : Int,
+	bytesPerRow : Int,
+	redMask : Int,
+	greenMask : Int,
+	blueMask : Int,
+	alphaMask : Int,
+	redShift : Int,
+	greenShift : Int,
+	blueShift : Int,
+	alphaShift : Int
+}
+
+class ClipboardImage {
+	public var spec : ClipboardImageSpec;
+	public var data : hl.Bytes;
+
+	public function new (data, spec) {
+		this.data = data;
+		this.spec = spec;
+	}
+}
+
+@:hlNative("os")
+class Clipboard {
+	static function get_clipboard_text():hl.Bytes {
+		return null;
+	}
+
+	public static function getClipboardText() {
+		return @:privateAccess String.fromUTF8(get_clipboard_text());
+	}
+
+	static function set_clipboard_text(text:hl.Bytes):Bool {
+		return false;
+	}
+
+	public static function setClipboardText(text:String) {
+		return @:privateAccess set_clipboard_text(text.toUtf8());
+	}
+
+	@:hlNative("os", "get_clipboard_image_data")
+	static function getClipboardImageData() : hl.Bytes {
+		return null;
+	}
+
+	public static function getClipboardImage () : ClipboardImage {
+		return new ClipboardImage(getClipboardImageData(), getClipboardImageSpec());
+	}
+	
+	static function set_clipboard_image(data : hl.Bytes, w : Int, h : Int, bpp : Int, bpr : Int, rmask : Int, gmask : Int, bmask : Int, amask : Int, rshift : Int, gshift : Int, bshift : Int, ashift : Int) : Bool {
+		return false;
+	}
+
+	public static function setClipboardImage(imageData:hl.Bytes, imageSpec : ClipboardImageSpec) {
+		return set_clipboard_image(
+			imageData, imageSpec.width, imageSpec.height,
+			imageSpec.bitsPerPixel, imageSpec.bytesPerRow,
+			imageSpec.redMask, imageSpec.greenMask, imageSpec.blueMask, imageSpec.alphaMask,
+			imageSpec.redShift, imageSpec.greenShift, imageSpec.blueShift, imageSpec.alphaShift
+		);
+	}
+
+	static function get_clipboard_image_spec (w : hl.Ref<Int>, h : hl.Ref<Int>, bpp : hl.Ref<Int>, bpr : hl.Ref<Int>, rmask : hl.Ref<Int>, gmask : hl.Ref<Int>, bmask : hl.Ref<Int>, amask : hl.Ref<Int>, rshift : hl.Ref<Int>, gshift : hl.Ref<Int>, bshift : hl.Ref<Int>, ashift : hl.Ref<Int>) : Bool {
+		return false;
+	}
+
+	public static function getClipboardImageSpec() : ClipboardImageSpec {
+		var width = 0, height = 0, bitsPerPixel = 0, bytesPerRow = 0, redMask = 0, greenMask = 0, blueMask = 0, alphaMask = 0, 
+			redShift = 0, greenShift = 0, blueShift = 0, alphaShift = 0;
+
+		get_clipboard_image_spec(width, height, bitsPerPixel, bytesPerRow, redMask, greenMask, blueMask, alphaMask, redShift, greenShift, blueShift, alphaShift);
+
+		return {
+			width : width, height : height,
+			bitsPerPixel : bitsPerPixel, bytesPerRow : bytesPerRow,
+			redMask : redMask, greenMask : greenMask, blueMask : blueMask, alphaMask : alphaMask,
+			redShift : redShift, greenShift : greenShift, blueShift : blueShift, alphaShift : alphaShift
+		};
+	}
+
+}
