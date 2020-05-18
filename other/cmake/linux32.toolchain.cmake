d6212e2f9b553d4c86c67eb356a5ee0b3ecc27fc
Taneli Heikkinen <trikafka@gmail.com>
2020-05-18 20:03:48 +0300

    Let's try this again...

--- other/cmake/linux32.toolchain.cmake
+++ other/cmake/linux32.toolchain.cmake
@@ -8,3 +8,4 @@ set(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_CXX_FLAGS     "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS       "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
+set(CMAKE_CXX_COMPILER  "g++")
