13d6169e6e61fd6537b0b049576e8583f8539e0d
Aleksandr Kuzmenko <alex@stablex.ru>
2021-08-27 07:53:45 +0300

    uvsample

--- other/uvsample/UVSample.hx
+++ other/uvsample/UVSample.hx
@@ -1,10 +1,11 @@
 class UVSample {
 	static function main() {
+		run();
 		// CheckSample.main();
 		// TcpSample.main();
 		// DnsSample.main();
 		// UdpSample.main();
-		PipeSample.main();
+		// PipeSample.main();
 		// ProcessSample.main();
 		// FileSample.main();
 		// DirSample.main();
@@ -13,5 +14,18 @@ class UVSample {
 		// MiscSample.main();
 		// TtySample.main();
 		// VersionSample.main();
+	}
+
+	macro static public function run() {
+		var sample = haxe.macro.Context.definedValue('SAMPLE');
+		if(sample == null || sample == '')
+			sample = Sys.getEnv('SAMPLE');
+		if(sample == null || sample == '') {
+			Sys.println('Add -D SAMPLE=<sample> or set environment variable SAMPLE=<sample>');
+			Sys.println('\twhere <sample> is a sample name. For example: SAMPLE=Tcp');
+			Sys.exit(1);
+		}
+		sample += 'Sample';
+		return macro $i{sample}.main();
 	}
 }
