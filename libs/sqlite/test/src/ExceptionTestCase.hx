187cccea72e37753a1a3ad4fc3afda87ce98356e
tobil4sk <tobil4sk@outlook.com>
2022-02-18 20:44:36 +0000

    Add test case for sqlite lib

--- libs/sqlite/test/src/ExceptionTestCase.hx
+++ libs/sqlite/test/src/ExceptionTestCase.hx
@@ -10,35 +10,35 @@ class ExceptionTestCase extends TestCase
 {
 	var file : String;
 	var cnx : Connection;
-	
+
 	override public function setup( ) : Void
 	{
 		super.setup();
-		
+
 		file = '${currentTest.classname}-${currentTest.method}.sqlite';
 		cnx = Sqlite.open(file);
-		
+
 		cnx.request('DROP TABLE IF EXISTS t1');
 		cnx.request('CREATE TABLE t1(i int, f float, t text, bl blob, bo bool)');
-		
+
 		cnx.request('INSERT INTO  t1 VALUES(1, 0.1, "hello", "\x11", 1)');
 		cnx.request('INSERT INTO  t1 VALUES(2, 0.00002, "goodbye", "\x1111", 0)');
 		cnx.request('INSERT INTO  t1 VALUES(3, 0.000000003, "Привет!", "\x111111", 1)');
 	}
-	
+
 	override public function tearDown( ) : Void
 	{
 		super.tearDown();
-		
+
 		cnx.close();
 		FileSystem.deleteFile(file);
 	}
-	
+
 	public function testSqlError( ) : Void
 	{
 		var sql : String = 'SELECT * FRM t1';
 		var expected : String = 'SQLite error: near "FRM": syntax error';
-		
+
 		var msg : String = null;
 		try
 		{
@@ -48,15 +48,15 @@ class ExceptionTestCase extends TestCase
 		{
 			msg = Std.string(e);
 		}
-		
+
 		assertEquals(expected, msg);
 	}
-	
+
 	public function testSameField( ) : Void
 	{
 		var sql : String = 'SELECT i as a, f as a FROM t1';
 		var expected : String = 'SQLite error: Same field is two times in the request: $sql';
-		
+
 		var msg : String = null;
 		try
 		{
@@ -66,7 +66,7 @@ class ExceptionTestCase extends TestCase
 		{
 			msg = Std.string(e);
 		}
-		
+
 		assertEquals(expected, msg);
 	}
 }
