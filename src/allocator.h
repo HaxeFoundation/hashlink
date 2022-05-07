ae14a58d95a1de6d0e1c86913fedc7b05fc8c502
Nicolas Cannasse <ncannasse@gmail.com>
2022-05-07 16:28:42 +0200

    rework gc to use flushed free lists instead of mark bits at allocation.

--- src/allocator.h
+++ src/allocator.h
@@ -1,11 +1,24 @@
 
 typedef struct {
+	int pos;
+	int count;
+} gc_fl;
+
+typedef struct _gc_freelist {
+	int current;
+	int count;
+	int size_bits;
+	struct _gc_freelist *cached_next;
+	gc_fl first;
+} gc_freelist;
+
+typedef struct {
 	int block_size;
 	int max_blocks;
 	int first_block;
+	bool need_flush;
 	// mutable
-	int next_block;
-	int free_blocks;
+	gc_freelist *free;
 	unsigned char *sizes;
 	int sizes_ref;
 	int sizes_ref2;
