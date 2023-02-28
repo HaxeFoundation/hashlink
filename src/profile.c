caf54d926d1c8d41ef4823ace7a1e30cb829671c
Ryan Cleven <rcleven@mac.com>
2023-02-28 11:36:17 -0800

    Retreived mac stack pointer

--- src/profile.c
+++ src/profile.c
@@ -152,6 +152,17 @@ static void *get_thread_stackptr( thread_handle *t, void **eip ) {
 	*eip = (void*)shared_context.context.uc_mcontext.gregs[REG_EIP];
 	return (void*)shared_context.context.uc_mcontext.gregs[REG_ESP];
 #	endif
+#elif defined(HL_MAC)
+#	ifdef HL_64
+	struct __darwin_mcontext64 *mcontext = shared_context.context.uc_mcontext;
+	if (mcontext != NULL) {
+		*eip = (void*)mcontext->__ss.__rip;
+		return (void*)mcontext->__ss.__rsp;
+	}
+	return NULL;
+#	else
+	return NULL;
+#	endif
 #else
 	return NULL;
 #endif
