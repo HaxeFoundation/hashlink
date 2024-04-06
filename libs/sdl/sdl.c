e8a2ac1a151ab44609c0cd9aa32ea942b20ab25b
Ryan Cleven <rcleven@mac.com>
2024-04-06 05:50:29 -0700

    Added vulkan support to more platforms
    Required adding sdl_vulkan.h

--- libs/sdl/sdl.c
+++ libs/sdl/sdl.c
@@ -5,6 +5,7 @@
 
 #if defined(_WIN32) || defined(__ANDROID__) || defined(HL_IOS) || defined(HL_TVOS)
 #	include <SDL.h>
+#	include <SDL_vulkan.h>
 #	include <SDL_syswm.h>
 #else
 #	include <SDL2/SDL.h>
@@ -515,16 +516,6 @@ DEFINE_PRIM(_BOOL, hint_value, _BYTES _BYTES);
 
 HL_PRIM SDL_Window *HL_NAME(win_create_ex)(int x, int y, int width, int height, int sdlFlags) {
 	// force window to match device resolution on mobile
-#ifdef	HL_MOBILE
-	SDL_DisplayMode displayMode;
-	SDL_GetDesktopDisplayMode(0, &displayMode);
-	SDL_Window* win = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | sdlFlags);
-#else
-#	ifdef HL_WIN
-	// Required for backwards comapatibility with older versions of hashlink
-	sdlFlags |= SDL_WINDOW_OPENGL;
-#else
-	// Only mac and windows support vulkan.
 	if (sdlFlags & (
 #ifdef HL_MAC
 		SDL_WINDOW_METAL | 
@@ -532,7 +523,12 @@ HL_PRIM SDL_Window *HL_NAME(win_create_ex)(int x, int y, int width, int height, 
 		SDL_WINDOW_VULKAN ) == 0) {
 		sdlFlags |= SDL_WINDOW_OPENGL;
 	}
-#endif
+
+#ifdef	HL_MOBILE
+	SDL_DisplayMode displayMode;
+	SDL_GetDesktopDisplayMode(0, &displayMode);
+	SDL_Window* win = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_BORDERLESS | sdlFlags);
+#else
 	SDL_Window* win = SDL_CreateWindow("", x, y, width, height, sdlFlags);
 #endif
 #	ifdef HL_WIN
