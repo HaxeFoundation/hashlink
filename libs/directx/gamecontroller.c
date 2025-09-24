5ea3a69ab507406bfc967e65d14bc7974ad4933b
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-09-24 10:45:13 -0500

    [dx] Add missing cast in gamecontroller
    Required by mingw compiler

--- libs/directx/gamecontroller.c
+++ libs/directx/gamecontroller.c
@@ -122,7 +122,7 @@ static LRESULT CALLBACK gctrl_WndProc(HWND wnd, UINT umsg, WPARAM wparam, LPARAM
 
 static void gctrl_dinput_init( varray *mappings ) {
 	if( !gctrl_dinput) {
-		DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, &IID_IDirectInput8, &gctrl_dinput, NULL);
+		DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, &IID_IDirectInput8, (LPVOID*)&gctrl_dinput, NULL);
 		if (gctrl_dinput) {
 			WNDCLASSEX wc;
 			memset(&wc, 0, sizeof(wc));
