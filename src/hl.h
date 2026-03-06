HaxeFoundation/hashlink

commit 043fc84db5259885325d0d431bc2481ecad9ed58

reorganiser the different register and callbacks setups into a single hl_setup structure

src/hl.h
@@ -595,6 +595,39 @@ HL_API hl_type hlt_dynobj;
 HL_API hl_type hlt_bool;
 HL_API hl_type hlt_abstract;
 
+
+
+#ifdef HL_WIN
+typedef uchar pchar;
+#define pstrchr wcschr
+#define pstrlen	ustrlen
+#else
+typedef char pchar;
+#define pstrchr strchr
+#define pstrlen	strlen
+#define HL_UTF8PATH
+#endif
+
+
+typedef struct {
+	pchar* file_path;
+	pchar** sys_args;
+	int sys_nargs;
+	void (*throw_jump)(jmp_buf, int);
+	uchar* (*resolve_symbol)(void* addr, uchar* out, int* outSize);
+	int (*capture_stack)(void** stack, int size);
+	bool (*reload_check)(vbyte* alt_file);
+	void* (*static_call)(void* fun, hl_type* t, void** args, vdynamic* out);
+	void* (*get_wrapper)(hl_type* t);
+	bool static_call_ref;
+	int closure_stack_capture;
+	bool is_debugger_enabled;
+	bool is_debugger_attached;
+} hl_setup_t;
+
+HL_API hl_setup_t hl_setup;
+HL_API void hl_sys_init();
+
 HL_API double hl_nan( void );
 HL_API bool hl_is_dynamic( hl_type *t );
 #define hl_is_ptr(t)	((t)->kind >= HBYTES)
@@ -632,13 +665,10 @@ HL_API void hl_assert( void );
 HL_API HL_NO_RETURN( void hl_throw( vdynamic *v ) );
 HL_API HL_NO_RETURN( void hl_rethrow( vdynamic *v ) );
 HL_API HL_NO_RETURN( void hl_null_access( void ) );
-HL_API void hl_setup_longjump( void *j );
-HL_API void hl_setup_exception( void *resolve_symbol, void *capture_stack );
 HL_API void hl_dump_stack( void );
 HL_API void hl_print_uncaught_exception( vdynamic *exc );
 HL_API varray *hl_exception_stack( void );
 HL_API bool hl_detect_debugger( void );
-HL_API void hl_set_debug_mode( bool b );
 
 HL_API vvirtual *hl_to_virtual( hl_type *vt, vdynamic *obj );
 HL_API void hl_init_virtual( hl_type *vt, hl_module_context *ctx );
@@ -895,10 +925,6 @@ typedef struct {
 #define hl_fatal4(msg,p0,p1,p2,p3)	hl_fatal_fmt(__FILE__,__LINE__,msg,p0,p1,p2,p3)
 HL_API void *hl_fatal_error( const char *msg, const char *file, int line );
 HL_API void hl_fatal_fmt( const char *file, int line, const char *fmt, ...);
-HL_API void hl_sys_init(void **args, int nargs, void *hlfile);
-HL_API void hl_setup_callbacks(void *sc, void *gw);
-HL_API void hl_setup_callbacks2(void *sc, void *gw, int flags);
-HL_API void hl_setup_reload_check( void *freload, void *param );
 
 #include <setjmp.h>
 typedef struct _hl_trap_ctx hl_trap_ctx;

src/hlc_main.c
@@ -163,9 +163,13 @@ int main(int argc, char *argv[]) {
 	sys_global_init();
 	hl_global_init();
 	hl_register_thread(&ret);
-	hl_setup_exception(hlc_resolve_symbol,hlc_capture_stack);
-	hl_setup_callbacks(hlc_static_call, hlc_get_wrapper);
-	hl_sys_init((void**)(argv + 1),argc - 1,NULL);
+	hl_setup.resolve_symbol = hlc_resolve_symbol;
+	hl_setup.capture_stack = hlc_capture_stack;
+	hl_setup.static_call = hlc_static_call;
+	hl_setup.get_wrapper = hlc_get_wrapper;
+	hl_setup.sys_args = (pchar**)(argv + 1);
+	hl_setup.sys_nargs = argc - 1;
+	hl_sys_init();
 	tf.ret = &hlt_void;
 	clt.kind = HFUN;
 	clt.fun = &tf;

src/main.c
@@ -89,13 +89,16 @@ static hl_code *load_code( const pchar *file, char **error_msg, bool print_error
 	return code;
 }
 
-static bool check_reload( main_context *m ) {
-	int time = pfiletime(m->file);
+static main_context* reload_ctx = NULL;
+static bool check_reload( vbyte *alt_file ) {
+	main_context* m = reload_ctx;
+	pchar* file = alt_file ? (pchar*)alt_file : m->file;
+	int time = pfiletime(file);
 	bool changed;
 	if( time == m->file_time )
 		return false;
 	char *error_msg = NULL;
-	hl_code *code = load_code(m->file, &error_msg, false);
+	hl_code *code = load_code(file, &error_msg, false);
 	if( code == NULL )
 		return false;
 	changed = hl_module_patch(m->m, code);
@@ -213,7 +216,10 @@ int main(int argc, pchar *argv[]) {
 		}
 	}
 	hl_global_init();
-	hl_sys_init((void**)argv,argc,file);
+	hl_setup.file_path = file;
+	hl_setup.sys_args = (pchar**)argv;
+	hl_setup.sys_nargs = argc;
+	hl_sys_init();
 	hl_register_thread(&ctx);
 	ctx.file = file;
 	ctx.code = load_code(file, &error_msg, true);
@@ -228,7 +234,8 @@ int main(int argc, pchar *argv[]) {
 		return 3;
 	if( hot_reload ) {
 		ctx.file_time = pfiletime(ctx.file);
-		hl_setup_reload_check(check_reload,&ctx);
+		reload_ctx = &ctx;
+		hl_setup.reload_check = check_reload;
 	}
 	hl_code_free(ctx.code);
 	if( debug_port > 0 && !hl_module_debug(ctx.m,debug_port,debug_wait) ) {

src/std/error.c
@@ -48,29 +48,8 @@ HL_PRIM void *hl_fatal_error( const char *msg, const char *file, int line ) {
 	return NULL;
 }
 
-typedef uchar *(*resolve_symbol_type)( void *addr, uchar *out, int *outSize );
-typedef int (*capture_stack_type)( void **stack, int size );
-
-static resolve_symbol_type resolve_symbol_func = NULL;
-static capture_stack_type capture_stack_func = NULL;
-
-int hl_internal_capture_stack( void **stack, int size ) {
-	return capture_stack_func(stack,size);
-}
-
 HL_PRIM uchar *hl_resolve_symbol( void *addr, uchar *out, int *outSize ) {
-	return resolve_symbol_func(addr, out, outSize);
-}
-
-static void (*throw_jump)( jmp_buf, int ) = NULL;
-
-HL_PRIM void hl_setup_longjump( void *j ) {
-	throw_jump = j;
-}
-
-HL_PRIM void hl_setup_exception( void *resolve_symbol, void *capture_stack ) {
-	resolve_symbol_func = resolve_symbol;
-	capture_stack_func = capture_stack;
+	return hl_setup.resolve_symbol(addr, out, outSize);
 }
 
 HL_PRIM void hl_set_error_handler( vclosure *d ) {
@@ -79,20 +58,10 @@ HL_PRIM void hl_set_error_handler( vclosure *d ) {
 	t->exc_handler = d;
 }
 
-#ifdef HL_DEBUG
-static bool hl_is_debug_mode = true;
-#else
-static bool hl_is_debug_mode = false;
-#endif
-
-HL_PRIM void hl_set_debug_mode( bool b ) {
-	hl_is_debug_mode = b;
-}
-
 static bool break_on_trap( hl_thread_info *t, hl_trap_ctx *trap, vdynamic *v ) {
 	bool unwrapped = false;
 	vdynamic *vvalue = NULL;
-	if( !hl_is_debug_mode ) return false;
+	if( !hl_setup.is_debugger_attached ) return false;
 	while( true ) {
 		if( trap == NULL || trap == t->trap_uncaught || t->trap_current == NULL || trap->prev == NULL ) return true;
 		if( !trap->tcheck || !v ) return false;
@@ -120,7 +89,7 @@ HL_PRIM void hl_throw( vdynamic *v ) {
 	if( t->flags & HL_EXC_KILL )
 		hl_fatal("Exception Occured");
 	if( !(t->flags & HL_EXC_RETHROW) )
-		t->exc_stack_count = capture_stack_func(t->exc_stack_trace, HL_EXC_MAX_STACK);
+		t->exc_stack_count = hl_setup.capture_stack(t->exc_stack_trace, HL_EXC_MAX_STACK);
 	t->exc_value = v;
 	t->trap_current = trap->prev;
 	call_handler = trap == t->trap_uncaught || t->trap_current == NULL;
@@ -132,8 +101,8 @@ HL_PRIM void hl_throw( vdynamic *v ) {
 	if( trap == t->trap_uncaught ) t->trap_uncaught = NULL;
 	t->flags &= ~HL_EXC_RETHROW;
 	if( t->exc_handler && call_handler ) hl_dyn_call_safe(t->exc_handler,&v,1,&call_handler);
-	if( throw_jump == NULL ) throw_jump = longjmp;
-	throw_jump(trap->buf,1);
+	if( hl_setup.throw_jump == NULL ) hl_setup.throw_jump = longjmp;
+	hl_setup.throw_jump(trap->buf,1);
 	HL_UNREACHABLE;
 }
 
@@ -150,13 +119,13 @@ HL_PRIM void hl_throw_buffer( hl_buffer *b ) {
 
 HL_PRIM void hl_dump_stack() {
 	void *stack[0x1000];
-	int count = capture_stack_func(stack, 0x1000);
+	int count = hl_setup.capture_stack(stack, 0x1000);
 	int i;
 	for(i=0;i<count;i++) {
 		void *addr = stack[i];
 		uchar sym[512];
 		int size = 512;
-		uchar *str = resolve_symbol_func(addr, sym, &size);
+		uchar *str = hl_setup.resolve_symbol(addr, sym, &size);
 		if( str == NULL ) {
 			int iaddr = (int)(int_val)addr;
 			usprintf(sym,512,USTR("@0x%X"),iaddr);
@@ -205,7 +174,7 @@ HL_PRIM varray *hl_exception_stack() {
 		void *addr = t->exc_stack_trace[i];
 		uchar sym[512];
 		int size = 512;
-		uchar *str = resolve_symbol_func(addr, sym, &size);
+		uchar *str = hl_setup.resolve_symbol(addr, sym, &size);
 		if( str == NULL ) continue;
 		hl_aptr(a,vbyte*)[pos++] = hl_copy_bytes((vbyte*)str,sizeof(uchar)*(size+1));
 	}
@@ -221,8 +190,8 @@ HL_PRIM int hl_exception_stack_raw( varray *arr ) {
 
 HL_PRIM int hl_call_stack_raw( varray *arr ) {
 	if( !arr )
-		return capture_stack_func(NULL,0);
-	return capture_stack_func(hl_aptr(arr,void*), arr->size);
+		return hl_setup.capture_stack(NULL,0);
+	return hl_setup.capture_stack(hl_aptr(arr,void*), arr->size);
 }
 
 HL_PRIM void hl_rethrow( vdynamic *v ) {

src/std/fun.c
@@ -22,8 +22,6 @@
 #include <hl.h>
 #include "hlsystem.h"
 
-HL_PRIM int hl_closure_stack_capture = 0;
-
 static void fun_var_args() {
 	hl_error("Variable fun args was not cast to typed function");
 }
@@ -51,17 +49,15 @@ static hl_type *hl_get_closure_type( hl_type *t ) {
 	return (hl_type*)&ft->closure_type;
 }
 
-int hl_internal_capture_stack( void **stack, int size );
-
 HL_PRIM vclosure *hl_alloc_closure_ptr( hl_type *fullt, void *fvalue, void *v ) {
 	hl_type *t = hl_get_closure_type(fullt);
-	vclosure *c = (vclosure*)hl_gc_alloc(t, sizeof(vclosure) + sizeof(void*) * hl_closure_stack_capture);
+	vclosure *c = (vclosure*)hl_gc_alloc(t, sizeof(vclosure) + sizeof(void*) * hl_setup.closure_stack_capture);
 	c->t = t;
 	c->fun = fvalue;
 	c->hasValue = 1;
 #	ifdef HL_64
 	int stack = 0;
-	if( hl_closure_stack_capture ) stack = hl_internal_capture_stack((void**)(c + 1), hl_closure_stack_capture);
+	if( hl_setup.closure_stack_capture ) stack = hl_setup.capture_stack((void**)(c + 1), hl_setup.closure_stack_capture);
 	c->stackCount = stack;
 #	endif
 	c->value = v;
@@ -116,24 +112,6 @@ HL_PRIM bool hl_fun_compare( vdynamic *a, vdynamic *b ) {
 
 
 // ------------ DYNAMIC CALLS
-
-typedef void *(*fptr_static_call)(void *fun, hl_type *t, void **args, vdynamic *out);
-typedef void *(*fptr_get_wrapper)(hl_type *t);
-
-static fptr_static_call hlc_static_call = NULL;
-static fptr_get_wrapper hlc_get_wrapper = NULL;
-static int hlc_call_flags = 0;
-
-HL_PRIM void hl_setup_callbacks2( void *c, void *w, int flags ) {
-	hlc_static_call = (fptr_static_call)c;
-	hlc_get_wrapper = (fptr_get_wrapper)w;
-	hlc_call_flags = flags;
-}
-
-HL_PRIM void hl_setup_callbacks( void *c, void *w ) {
-	hl_setup_callbacks2(c,w,0);
-}
-
 
 #define HL_MAX_ARGS 9
 
@@ -198,7 +176,7 @@ HL_PRIM vdynamic* hl_call_method( vdynamic *c, varray *args ) {
 		}
 		pargs[i] = p;
 	}
-	ret = hlc_static_call(hlc_call_flags & 1 ? &cl->fun : cl->fun,cl->t,pargs,&out);
+	ret = hl_setup.static_call(hl_setup.static_call_ref ? &cl->fun : cl->fun,cl->t,pargs,&out);
 	tret = cl->t->fun->ret;
 	if( !hl_is_ptr(tret) ) {
 		vdynamic *r;
@@ -306,7 +284,7 @@ HL_PRIM void *hl_wrapper_call( void *_c, void **args, vdynamic *ret ) {
 			vargs[p++] = v;
 		}
 	}
-	pret = hlc_static_call(hlc_call_flags & 1 ? &w->fun : w->fun,w->hasValue ? w->t->fun->parent : w->t,vargs,ret);
+	pret = hl_setup.static_call(hl_setup.static_call_ref ? &w->fun : w->fun,w->hasValue ? w->t->fun->parent : w->t,vargs,ret);
 	aret = hl_is_ptr(w->t->fun->ret) ? &pret : pret;
 	if( aret == NULL ) aret = &pret;
 	switch( tfun->ret->kind ) {
@@ -346,7 +324,7 @@ HL_PRIM void *hl_dyn_call_obj( vdynamic *o, hl_type *ft, int hfield, void **args
 			if( tmp ) {
 				vclosure_wrapper w;
 				w.cl.t = ft;
-				w.cl.fun = hlc_get_wrapper(ft);
+				w.cl.fun = hl_setup.get_wrapper(ft);
 				w.cl.hasValue = 2;
 #				ifdef HL_64
 				w.cl.stackCount = 0;
@@ -369,7 +347,7 @@ HL_PRIM void *hl_dyn_call_obj( vdynamic *o, hl_type *ft, int hfield, void **args
 					vclosure_wrapper w;
 					vclosure tmp;
 					w.cl.t = ft;
-					w.cl.fun = hlc_get_wrapper(ft);
+					w.cl.fun = hl_setup.get_wrapper(ft);
 					w.cl.hasValue = 2;
 #					ifdef HL_64
 					w.cl.stackCount = 0;
@@ -407,7 +385,7 @@ HL_PRIM void *hl_dyn_call_obj( vdynamic *o, hl_type *ft, int hfield, void **args
 
 HL_PRIM vclosure *hl_make_fun_wrapper( vclosure *v, hl_type *to ) {
 	vclosure_wrapper *c;
-	void *wrap = hlc_get_wrapper(to);
+	void *wrap = hl_setup.get_wrapper(to);
 	if( wrap == NULL ) return NULL;
 	if( v->fun != fun_var_args && v->t->fun->nargs != to->fun->nargs )
 		return NULL;

src/std/sys.c
@@ -62,20 +62,9 @@ typedef struct _stat32 pstat;
 #	include <sys/times.h>
 #	include <sys/wait.h>
 #	include <locale.h>
-#	define HL_UTF8PATH
 typedef struct stat pstat;
 #endif
 
-#endif
-
-#ifdef HL_UTF8PATH
-typedef char pchar;
-#define pstrchr strchr
-#define pstrlen	strlen
-#else
-typedef uchar pchar;
-#define pstrchr wcschr
-#define pstrlen	ustrlen
 #endif
 
 #ifdef HL_MAC
@@ -87,6 +76,8 @@ typedef uchar pchar;
 #ifndef CLK_TCK
 #	define CLK_TCK	100
 #endif
+
+HL_PRIM hl_setup_t hl_setup = { 0 };
 
 static pchar *pstrdup( const pchar *s, int len ) {
 	pchar *ret;
@@ -674,53 +665,33 @@ HL_PRIM int hl_sys_get_char( bool b ) {
 #	endif
 }
 
-static pchar **sys_args;
-static int sys_nargs;
-
 HL_PRIM varray *hl_sys_args() {
-	varray *a = hl_alloc_array(&hlt_bytes,sys_nargs);
+	varray *a = hl_alloc_array(&hlt_bytes,hl_setup.sys_nargs);
 	int i;
-	for(i=0;i<sys_nargs;i++)
-		hl_aptr(a,pchar*)[i] = sys_args[i];
+	for(i=0;i<hl_setup.sys_nargs;i++)
+		hl_aptr(a,pchar*)[i] = hl_setup.sys_args[i];
 	return a;
 }
 
-static void *hl_file = NULL;
-
-HL_PRIM void hl_sys_init(void **args, int nargs, void *hlfile) {
+HL_PRIM void hl_sys_init() {
 #ifdef HL_WIN
 	QueryPerformanceFrequency(&qpcFrequency);
 #endif
-	sys_args = (pchar**)args;
-	sys_nargs = nargs;
-	hl_file = hlfile;
 #	ifdef HL_WIN_DESKTOP
 	setlocale(LC_CTYPE, ""); // printf to current locale
 #	endif
 }
 
 HL_PRIM vbyte *hl_sys_hl_file() {
-	return (vbyte*)hl_file;
-}
-
-static void *reload_fun = NULL;
-static void *reload_param = NULL;
-HL_PRIM void hl_setup_reload_check( void *freload, void *param ) {
-	reload_fun = freload;
-	reload_param = param;
+	return (vbyte*)hl_setup.file_path;
 }
 
 HL_PRIM bool hl_sys_check_reload( vbyte *debug_alt_file ) {
-	if( debug_alt_file && reload_param ) {
-		*((vbyte**)reload_param) = debug_alt_file;
-	}
-	return reload_fun && ((bool(*)(void*))reload_fun)(reload_param);
+	return hl_setup.reload_check != NULL && hl_setup.reload_check(debug_alt_file);
 }
 
-extern int hl_closure_stack_capture;
-
 HL_PRIM bool hl_sys_has_debugger() {
-	return hl_closure_stack_capture != 0;
+	return hl_setup.is_debugger_attached;
 }
 
 #ifndef HL_MOBILE

src/debugger.c
@@ -35,8 +35,6 @@ HL_API int hl_socket_recv( hl_socket *s, vbyte *buf, int pos, int len );
 HL_API void hl_sys_sleep( double t );
 HL_API int hl_sys_getpid();
 
-HL_API int hl_closure_stack_capture;
-
 static hl_socket *debug_socket = NULL;
 static hl_socket *client_socket = NULL;
 static bool debugger_connected = false;
@@ -103,7 +101,8 @@ static void hl_debug_loop( hl_module *m ) {
 			send(d->offsets,(d->large ? sizeof(int) : sizeof(unsigned short)) * (f->nops + 1));
 		}
 
-		hl_closure_stack_capture = 8;
+		hl_setup.closure_stack_capture = 8;
+		hl_setup.is_debugger_attached = true;
 
 		// wait answer
 		// for some reason, this is not working on windows (recv returns 0 ?)
@@ -142,7 +141,7 @@ h_bool hl_module_debug( hl_module *m, int port, h_bool wait ) {
 	hl_socket_close(debug_socket);
 	debug_socket = NULL;
 #	endif
-	hl_set_debug_mode(true);
+	hl_setup.is_debugger_enabled = true;
 	return true;
 }
 

src/jit.c
@@ -4592,9 +4592,11 @@ void *hl_jit_code( jit_ctx *ctx, hl_module *m, int *codesize, hl_debug_infos **d
 	if( !call_jit_c2hl ) {
 		call_jit_c2hl = code + ctx->c2hl;
 		call_jit_hl2c = code + ctx->hl2c;
-		hl_setup_callbacks2(callback_c2hl, get_wrapper, 1);
+		hl_setup.get_wrapper = get_wrapper;
+		hl_setup.static_call = callback_c2hl;
+		hl_setup.static_call_ref = true;
 #		ifdef JIT_CUSTOM_LONGJUMP
-		hl_setup_longjump(code + ctx->longjump);
+		hl_setup.throw_jump = (void(*)(jmp_buf, int))(code + ctx->longjump);
 #		endif
 		int i;
 		for(i=0;i<(int)(sizeof(ctx->static_functions)/sizeof(void*));i++)

commit 3efa9155be89245e2aaf4611720f905a89f156c2

Restore setup functions for abi compat
This was broken in:
043fc84db5259885325d0d431bc2481ecad9ed58
e33d5f10d612a2726820c2344ed68964b6b5e368

src/hl.h
@@ -630,7 +630,7 @@ typedef struct {
 } hl_setup_t;
 
 HL_API hl_setup_t hl_setup;
-HL_API void hl_sys_init();
+HL_API void hl_sys_init( void **args, int nargs, void *hlfile );
 
 HL_API double hl_nan( void );
 HL_API bool hl_is_dynamic( hl_type *t );
@@ -1019,6 +1019,15 @@ HL_API hl_track_info hl_track;
 #define hl_track_call(a,b)
 
 #endif
+
+// -------------- COMPAT ------------------------------------
+
+HL_API void hl_setup_longjump( void *j );
+HL_API void hl_setup_exception( void *resolve_symbol, void *capture_stack );
+HL_API void hl_set_debug_mode( bool b );
+HL_API void hl_setup_callbacks(void* sc, void* gw);
+HL_API void hl_setup_callbacks2(void* sc, void* gw, int flags);
+HL_API void hl_setup_reload_check( void *freload, void *param );
 
 C_FUNCTION_END
 

src/hlc_main.c
@@ -160,7 +160,7 @@ int main(int argc, char *argv[]) {
 	hl_setup.get_wrapper = hlc_get_wrapper;
 	hl_setup.sys_args = (pchar**)(argv + 1);
 	hl_setup.sys_nargs = argc - 1;
-	hl_sys_init();
+	hl_sys_init(NULL, 0, NULL);
 	tf.ret = &hlt_void;
 	clt.kind = HFUN;
 	clt.fun = &tf;

src/main.c
@@ -272,7 +272,7 @@ int main(int argc, pchar *argv[]) {
 	hl_setup.file_path = file;
 	hl_setup.sys_args = (pchar**)argv;
 	hl_setup.sys_nargs = argc;
-	hl_sys_init();
+	hl_sys_init(NULL, 0, NULL);
 	hl_register_thread(&ctx);
 	main_ctx = &ctx;
 	ctx.file = file;

src/std/error.c
@@ -52,10 +52,25 @@ HL_PRIM uchar *hl_resolve_symbol( void *addr, uchar *out, int *outSize ) {
 	return hl_setup.resolve_symbol(addr, out, outSize);
 }
 
+static void (*throw_jump)( jmp_buf, int ) = NULL;
+
+HL_PRIM void hl_setup_longjump( void *j ) {
+	throw_jump = j;
+}
+
+HL_PRIM void hl_setup_exception( void *resolve_symbol, void *capture_stack ) {
+	hl_setup.resolve_symbol = resolve_symbol;
+	hl_setup.capture_stack = capture_stack;
+}
+
 HL_PRIM void hl_set_error_handler( vclosure *d ) {
 	hl_thread_info *t = hl_get_thread();
 	t->trap_uncaught = t->trap_current;
 	t->exc_handler = d;
+}
+
+HL_PRIM void hl_set_debug_mode( bool b ) {
+	hl_setup.is_debugger_enabled = b;
 }
 
 static bool break_on_trap( hl_thread_info *t, hl_trap_ctx *trap, vdynamic *v ) {

src/std/fun.c
@@ -113,6 +113,20 @@ HL_PRIM bool hl_fun_compare( vdynamic *a, vdynamic *b ) {
 
 // ------------ DYNAMIC CALLS
 
+typedef void *(*fptr_static_call)(void *fun, hl_type *t, void **args, vdynamic *out);
+typedef void *(*fptr_get_wrapper)(hl_type *t);
+
+HL_PRIM void hl_setup_callbacks2( void *c, void *w, int flags ) {
+	hl_setup.static_call = (fptr_static_call)c;
+	hl_setup.get_wrapper = (fptr_get_wrapper)w;
+	hl_setup.static_call_ref = flags & 1;
+}
+
+HL_PRIM void hl_setup_callbacks( void *c, void *w ) {
+	hl_setup.static_call = (fptr_static_call)c;
+	hl_setup.get_wrapper = (fptr_get_wrapper)w;
+}
+
 #define HL_MAX_ARGS 9
 
 HL_PRIM vdynamic* hl_call_method( vdynamic *c, varray *args ) {

src/std/sys.c
@@ -159,6 +159,11 @@ HL_PRIM void hl_sys_print( vbyte *msg ) {
 	hl_blocking(false);
 }
 
+HL_PRIM void hl_setup_profiler( void *profile_event, void *before_exit ) {
+	hl_setup.before_exit = before_exit;
+	hl_setup.profile_event = profile_event;
+}
+
 HL_PRIM void hl_sys_profile_event( int code, vbyte *data, int dataLen ) {
 	if( hl_setup.profile_event ) hl_setup.profile_event(code, data, dataLen);
 }
@@ -166,6 +171,18 @@ HL_PRIM void hl_sys_profile_event( int code, vbyte *data, int dataLen ) {
 HL_PRIM void hl_sys_exit( int code ) {
 	if( hl_setup.before_exit ) hl_setup.before_exit();
 	exit(code);
+}
+
+static void *f_vtune_init = NULL;
+static void *g_vtune_module = NULL;
+static void setup_vtune_wrapper() {
+	((void(*)(void*))f_vtune_init)(g_vtune_module);
+}
+
+HL_PRIM void hl_setup_vtune( void *vtune_init, void *m ) {
+	f_vtune_init = vtune_init;
+	g_vtune_module = m;
+	hl_setup.vtune_init = setup_vtune_wrapper;
 }
 
 HL_PRIM void hl_sys_vtune_init() {
@@ -674,10 +691,16 @@ HL_PRIM varray *hl_sys_args() {
 	return a;
 }
 
-HL_PRIM void hl_sys_init() {
+HL_PRIM void hl_sys_init(void **args, int nargs, void *hlfile) {
 #ifdef HL_WIN
 	QueryPerformanceFrequency(&qpcFrequency);
 #endif
+	if (hlfile)
+		hl_setup.file_path = (pchar*)hlfile;
+	if (args)
+		hl_setup.sys_args = (pchar**)args;
+	if (nargs)
+		hl_setup.sys_nargs = nargs;
 #	ifdef HL_WIN_DESKTOP
 	setlocale(LC_CTYPE, ""); // printf to current locale
 #	endif
@@ -685,6 +708,18 @@ HL_PRIM void hl_sys_init() {
 
 HL_PRIM vbyte *hl_sys_hl_file() {
 	return (vbyte*)hl_setup.file_path;
+}
+
+static void *compat_reload_fun = NULL;
+static void *compat_reload_param = NULL;
+static bool reload_wrapper() {
+	return ((bool(*)(void*))compat_reload_fun)(compat_reload_param);
+}
+
+HL_PRIM void hl_setup_reload_check( void *freload, void *param ) {
+	compat_reload_fun = freload;
+	compat_reload_param = param;
+	hl_setup.reload_check = reload_wrapper;
 }
 
 HL_PRIM bool hl_sys_check_reload( vbyte *debug_alt_file ) {