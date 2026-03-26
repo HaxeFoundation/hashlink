1ebb3c3b9a71440c7b2ce8433c0beb1e29f438ea
tobil4sk <tobil4sk@outlook.com>
2026-03-26 07:23:04 +0000

    [tool] Make hlc templates compatible with haxe ci (#916)
    * [tool] Set rpaths in makefile template
    
    * [tool] Support jumbo build in makefile template
    
    * [tool] Map jumbo build "1" to "true"
    
    This allows setting `-D hlgen.makefile.jumbo` to enable it
    
    * [tool] Default to multiple threads for make
    
    * [tool] Infer template for hlgen.makefile=1
    
    * [tool] Use object text file to avoid too many args
    
    Gcc on mingw fails due to too many arguments on very large builds.
    
    * [tool] Fix vs2022 template output directories
    
    It would create a folder like `main/x64/Release` instead of just using
    `x64/Release`. This caused issues because main is both a directory and
    main.exe is the executable.
    
    Now it matches vs2019, and explicitly places it in `x64/Release`.

--- other/haxelib/Run.hx
+++ other/haxelib/Run.hx
@@ -29,11 +29,17 @@ class Build {
 			Sys.println(message);
 	}
 
+	static function getDefaultTemplate() {
+		return (Sys.systemName() == "Windows") ? "vs2022" : "make";
+	}
 
 	public function generate() {
 		var tpl = config.defines.get("hlgen.makefile");
-		if( tpl != null )
+		if (tpl != null) {
+			if (tpl == "1")
+				tpl = config.defines["hlgen.makefile"] = getDefaultTemplate();
 			generateTemplates(tpl);
+		}
 		log('Code generated in $output');
 	}
 
@@ -45,7 +51,7 @@ class Build {
 					case 'x86_32': ["EXTRA_CFLAGS=-m32", "EXTRA_LDFLAGS=-m32"];
 					case _: [];
 				};
-				Sys.command("make", ["-C", targetDir].concat(platformArgs).concat(config.defines.exists("debug") ? ["DEBUG=1"] : []));
+				Sys.command("make", ["-j", "-C", targetDir].concat(platformArgs).concat(config.defines.exists("debug") ? ["DEBUG=1"] : []));
 			case "hxcpp":
 				var platformArgs = switch config.defines.get("hlgen.build.architecture") {
 					case 'x86_32': ["HXCPP_M32"];
@@ -83,8 +89,7 @@ class Build {
 				vswhereProc.close();
 				code;
 			case null:
-				var suggestion = (Sys.systemName() == "Windows") ? "vs2019" : "make";
-				log('Set -D hlgen.makefile=${suggestion} for automatic native compilation');
+				log('Set -D hlgen.makefile for automatic native compilation');
 				0;
 			case unimplemented:
 				log('Automatic native compilation not yet implemented for $unimplemented');
@@ -106,10 +111,11 @@ class Build {
 	}
 
 	function generateTemplates( ?tpl ) {
-		if( tpl == null || tpl == "1" )
-			tpl = "vs2015";
 		var srcDir = tpl;
-		var jumboBuild = config.defines.get("hlgen.makefile.jumbo");
+		var jumboBuild = switch config.defines.get("hlgen.makefile.jumbo") {
+			case "1": "true";
+			case value: value;
+		};
 		var relDir = "";
 		if( config.defines["hlgen.makefilepath"] != null ) {
 			targetDir = config.defines.get("hlgen.makefilepath");
